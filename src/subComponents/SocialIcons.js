b8d5c9d6001c4ecf9a8ce3d18a607b945c769875
Hotragn <hotragn.pettugani_2024@woxsen.edu.in>
2023-08-25 15:04:55 +0530

    fixed facebook

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 // import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
-import { Facebook, Github, Twitter, Linkedin} from "../components/AllSvgs";
+import { Github, Twitter, Linkedin} from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
 
 const Icons = styled.div`
@@ -67,24 +67,7 @@ const SocialIcons = (props) => {
           />
         </a>
       </motion.div>
-      <motion.div
-        initial={{scale:0 }}
-        animate={{ scale: [0, 1, 1.5, 1] }}
-        transition={{ type: "spring", duration: 1, delay: 1.4 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          rel="noreferrer"
-          href={"https://facebook.com/hotragn"}
-        >
-          <Facebook
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
+
       <motion.div
         initial={{scale:0 }}
         animate={{ scale: [0, 1, 1.5, 1] }}

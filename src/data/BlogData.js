b04e1c58490c003a5ea209cfc75106d94e716b79
Hotragn <103170876+Hotragn@users.noreply.github.com>
2024-10-26 04:50:14 -0400

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,6 +3,14 @@
 export const Blogs = [
     {
         id:1,
+        name:"Optimizing the E-Commerce Shopping Cart: A UI/UX Design Plan for seamless shopping",
+        tags:["UI/UX","Shopping Cart Enhancement","Redisigning","Analysis"],
+        date:"30 September, 2024",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1687186023289/5fe34e04-731e-4d48-bdbb-f8898fb726f9.avif?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://pettugani.hashnode.dev/data-science-riding-the-wave-of-innovation"
+    },
+    {
+        id:2,
         name:"Career Guidance System for Predicting the Job Role Using Machine Learning",
         tags:["MachineLearning","Python","Html","CSS","JavaScript"],
         date:"18 April, 2023",
@@ -10,7 +18,7 @@ export const Blogs = [
         link:"https://medium.com/@hotragn.vaishyas/career-guidance-system-for-predicting-the-job-role-using-machine-learning-bf4368b9f170"
     },
     {
-        id:2,
+        id:3,
         name:"Data Science: Riding the Wave of Innovation",
         tags:["Research","DataScience","Python","Analysis"],
         date:"19 June, 2023",

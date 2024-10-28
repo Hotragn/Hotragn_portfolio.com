90fc95b00f96efb6348b8d31180a4a3f8dc8ba39
Hotragn <103170876+Hotragn@users.noreply.github.com>
2024-10-27 21:03:34 -0400

    Added Pantrynex website to portfolio WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -10,4 +10,13 @@ export const Work = [
         demo:"https://github.com/Hotragn/Career-Guidance-System/tree/main/Machine_learning%20Model/User_Interface/career%20guidance%20UI",
         github:"https://github.com/Hotragn/Career-Guidance-System/tree/main"
     },
+    {
+        id:2,
+        name:"Pantry Manager",
+        description:"A platform allows you to organize, and effectively manage the pantry on daily basis",
+        tags:["Typescript",,"JavaScript","MaterialUI","CSS","FireBase"],
+        
+        demo:"https://pantrynex.vercel.app/",
+        github:"https://github.com/Hotragn/Pantry-Nex"
+    },
 ] 
